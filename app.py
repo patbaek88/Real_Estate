542bf43dbe614930d357b20d930dd1d2f1e64933
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-12-27 16:23:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ maxlags = st.number_input('몇개의 과거데이터로 예측 하겠습니까? 
 model = VAR(economic_data_norm)
 results = model.fit(maxlags=maxlags)  # 과거 12개월의 데이터를 사용하여 학습
 
-
+lag_selection = model.select_order(maxlags=maxlags)
+st.write(lag_selection.summary())
 
 
 # 미래 36개월(3년) 예측

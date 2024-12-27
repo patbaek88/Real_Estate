92553b0016e170fb5e53b87bad401990ffd73851
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-12-27 16:01:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 import seaborn as sns
 from statsmodels.tsa.vector_ar.var_model import VAR
-from statsmodels.tools.eval_measures import aic, bic
-form statsmodels.tsa.stattools import adfuller
+#from statsmodels.tools.eval_measures import aic, bic
+from statsmodels.tsa.stattools import adfuller
 import streamlit as st
 
 # 데이터 로드 및 전처리
@@ -41,7 +41,7 @@ maxlags = st.number_input('몇개의 6개월단위 과거데이터를 활용하�
 
 # VAR 모델 학습
 model = VAR(economic_data_norm)
-results = model.fit(maxlags=maxlags, ic='aic')  # 과거 12개월의 데이터를 사용하여 학습
+results = model.fit(maxlags=maxlags)  # 과거 12개월의 데이터를 사용하여 학습
 adf_test = adfuller(economic_data_norm)
 st.write("ADF Statistic:", adf_test[0])
 st.write("p-value:", adf_test[1])

2ba6d0cd6b0b8690bfb89cd139e9fcde760d39af
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-16 23:50:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -465,8 +465,10 @@ plt.plot(data_df_apt2.index, data_df_apt2['apt2_price'], label='Actual apt2_pric
 
 # 향후 36개월 동안 예측 시장가격 표시
 plt.scatter(predicted_my_land_price_df.index, predicted_my_land_price_df*69*2.5/100000000, label='Future_my_land_price', marker='^', color = "blue")
-plt.scatter(predicted_apt2_price_denorm2.index, predicted_apt2_price_denorm2 , label='Predicted_price_apt2', marker='x', color = "orange")
+plt.scatter(predicted_myland_price_denorm2_df.index, predicted_myland_price_denorm2_df, label='Predicted_my_land_price', marker='x', color = "blue")
+plt.scatter(predicted_apt2_price_denorm2.index, predicted_apt2_price_denorm2 , label='Future_price_apt2', marker='^', color = "orange")
 plt.scatter(predicted_apt2_price_denorm2_df.index, predicted_apt2_price_denorm2_df, label='Predicted_price_apt2', marker='x', color = "orange")
+
 
 
 plt.xlabel('Date')
